b4af9f048e93bc31445104309aec53fa84d332c7
ivan <ivangomezlanzat@gmail.com>
2024-04-01 19:45:38 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Logo</header>
+      <main>
+        <section className="section-main">
+          <p>
+            Si administras todo con un excel... Â¡Esta es tu app! Salufy te
+            ofrece una herrameinta para gestionar agenda, citas, reservas y
+            ademÃ¡s te conectamos con nuevos usuarios.
+          </p>
+          <p>Â¡Prueba ahora nuestra App y comienza a mejorar tu negocio!</p>
+          <button>Quiero probar la App</button>
+        </section>
+
+        <section>
+          <h2>Â¿CÃ³mo funciona?</h2>
+          <div>
+            <div>
+              <img></img>
+            </div>
+            <div>
+              <h4>Crea tu perfil profesional</h4>
+              <p>
+                Dile a los usuarios quÃ© servicios ofertas con todo tipo de
+                detalle. Titualciones, especialidades, tipo de pago, precios,
+                ubicaciones y muchas cosas mÃ¡s.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img></img>
+            </div>
+            <div>
+              <h4>Crea tu perfil profesional</h4>
+              <p>
+                Dile a los usuarios quÃ© servicios ofertas con todo tipo de
+                detalle. Titualciones, especialidades, tipo de pago, precios,
+                ubicaciones y muchas cosas mÃ¡s.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img></img>
+            </div>
+            <div>
+              <h4>Crea tu perfil profesional</h4>
+              <p>
+                Dile a los usuarios quÃ© servicios ofertas con todo tipo de
+                detalle. Titualciones, especialidades, tipo de pago, precios,
+                ubicaciones y muchas cosas mÃ¡s.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <div>
+              <h3>Empieza a descubrir los beneficios que aporta Salufy</h3>
+              <img></img>
+            </div>
+            <div>
+              <button>Empezar prueba gratis</button>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <img></img>
+            <h3>Recordatorios</h3>
+            <p>
+              No tendrÃ¡s que volver a recordar la fecha y hora de la sesiÃ³n a
+              tus usuarios. Salufy lo hace por tÃ­.
+            </p>
+          </div>
+          <div>
+            <img></img>
+            <h3>Recordatorios</h3>
+            <p>
+              No tendrÃ¡s que volver a recordar la fecha y hora de la sesiÃ³n a
+              tus usuarios. Salufy lo hace por tÃ­.
+            </p>
+          </div>
+          <div>
+            <img></img>
+            <h3>Recordatorios</h3>
+            <p>
+              No tendrÃ¡s que volver a recordar la fecha y hora de la sesiÃ³n a
+              tus usuarios. Salufy lo hace por tÃ­.
+            </p>
+          </div>
+          <div>
+            <img></img>
+            <h3>Recordatorios</h3>
+            <p>
+              No tendrÃ¡s que volver a recordar la fecha y hora de la sesiÃ³n a
+              tus usuarios. Salufy lo hace por tÃ­.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <h2>Â¿QuÃ© opinan nuestros profesionales</h2>
+          <div>Carrusel</div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
